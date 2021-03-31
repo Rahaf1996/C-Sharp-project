09831a5f33f77b3173c0933384e6d245a909c2e9
Rahaf1996 <64659405+Rahaf1996@users.noreply.github.com>
2021-03-31 16:13:11 +0300

    Add files via upload

---
+++ project/Project/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+namespace Project
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            string StudentName;
+            int StudentGread;
+            string Status;
+            string quit = "";
+
+            while (quit != "No")
+            {
+                Console.WriteLine("Would  you like to add a new student Yes or NO");
+                quit = Console.ReadLine();
+                if (quit == "yes")
+                {
+                    Console.WriteLine("Enter Student Name");
+                    StudentName = Console.ReadLine();
+                    Console.WriteLine("Enter Student Gread");
+                    StudentGread = int.Parse(Console.ReadLine());
+                    if (StudentGread >= 60)
+                    {
+                        Status = "prassed";
+                    }
+                    else {
+                        Status = "Filed";
+                         }
+
+                    Console.WriteLine("Student Name={0} \n Student Gread={1} \n status={2} \n ", StudentName, StudentGread, Status);
+
+
+                }
+                else
+                {
+                    Console.WriteLine("please Enter Yor N ");
+                }
+
+
+            }
+        }
+          
+    }
+}
